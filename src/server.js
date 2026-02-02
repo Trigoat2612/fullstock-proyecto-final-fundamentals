c05a3eabc0f7e6051888bac4a9538777893e1a8e
Miguel <mk_pl@hotmail.com>
2026-02-01 23:05:28 -0500

    Commit avance

--- src/server.js
+++ src/server.js
@@ -26,6 +26,31 @@ async function readJson() {
     }
 }
 
+async function getCart() {
+    try {
+        const content = await readFile(
+        path.join(__dirname, "cart.json"),
+        "utf8",
+        );
+        return JSON.parse(content);
+    } catch (error) {
+        console.log(error.message);
+        return null;
+    }
+}
+
+async function saveCart(content) {
+    try {
+        await writeFile(
+        path.join(__dirname, "cart.json"),
+        JSON.stringify(content,null,2),
+        );
+    } catch (error) {
+        console.log(error.message);
+        return null;
+    }
+}
+
 //Handlers
 async function homeHandler(_req, res) {
     const categories = await readJson();
@@ -39,9 +64,41 @@ async function categoryHandler(req, res) {
     const products = category.products;
     res.render("category", { category, products });
 }
+
+async function productHandler(req, res) {
+    const categories = await readJson();
+    const productId = Number(req.params.id);
+    let product = null;
+    for (const category of categories) {
+        product = category.products.find((p) => p.id === productId);
+        if (product) break;
+    }
+    if (product) {
+        res.render("product", { product });
+    } else {
+        res.status(404).send("Product not found");
+    }
+}
+
+async function addProductHandler(req, res) {
+    const cart = await getCart();
+    const categories = await readJson();
+    const productId = Number(req.params.id);
+    let product = null;
+    for (const category of categories) {
+        product = category.products.find((p) => p.id === productId);
+        if (product) break;
+    }
+    cart.push(product);
+    await saveCart(cart);
+    res.redirect(303,"/");
+}
 //Router
 app.get("/", homeHandler);
 app.get("/categories/:id", categoryHandler);
+app.get("/products/:id", productHandler);
+app.post("/cart/add/:id", addProductHandler);
+//app.get("/cart", cartHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
